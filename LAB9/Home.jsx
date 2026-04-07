c82dd588c9c74b3695b9faacdd6829c75235d4af
thanvid1 <tanvviiiiacharya@gmail.com>
2026-04-07 12:21:14 +0530

    Add Home component with welcome message and features

---
+++ LAB9/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{textAlign:"center", padding:"20px"}}>
+      <h2>Home Page</h2>
+      <p>Welcome to our website! This is the home page where you can find an overview of what we offer.</p>
+      
+      <h3>Features:</h3>
+      <ul>
+        <li>Easy navigation</li>
+        <li>Responsive design</li>
+        <li>User-friendly interface</li>
+      </ul>
+
+      <p>Explore the About and Contact pages to learn more about us.</p>
+    </div>
+  );
+}
+
+export default Home;
